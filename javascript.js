baa516c72cc0faef8f6781b2b3d008cd855c1ea2
Himanshu-Jaiswar <dilipjaiswar3323@gmail.com>
2024-11-03 14:59:09 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -38,20 +38,6 @@ reset.addEventListener('click', function (e) {
     document.getElementById('hr').textContent = "00"
 })
 
-let save = document.getElementById('save')
-let details = {}
-
-
-save.addEventListener('click', function(e){
-    let n = prompt("Name")
-
-    details[n] = h + " " + m  + " " + s + " " + mm + " "
-    
-    document.getElementById('list').innerHTML += `${n + details[n]} <br>`
-    
-})
-
-
 let milisecond = 0
 let second = 0
 let minute = 0
